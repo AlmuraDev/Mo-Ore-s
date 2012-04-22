7b4e0b870c7e6caf57bc107e4d669999b7ae971e
Zachary Lockwood <bestgoaltenderever@gmail.com>
2012-04-21 22:14:58 -0400

    added ores

--- src/com/github/Zarklord1/MoOres/MoOres.java
+++ src/com/github/Zarklord1/MoOres/MoOres.java
@@ -2,15 +2,25 @@ package com.github.Zarklord1.MoOres;
 
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.material.CustomBlock;
+import com.github.Zarklord1.MoOres.oreblocks.*;
 
 public class MoOres extends JavaPlugin{
 	
 	Logger log = Logger.getLogger("Minecraft");
+	public static CustomBlock Flariteore;
+	public static CustomBlock Silverore;
+	public static CustomBlock Rubyore;
+	public static CustomBlock Thermiteore;
+	public static CustomBlock Crystaliteore;
 	
 	public void onEnable(){ 
+		Flariteore = new Flariteore(this);
+		Silverore = new Silverore(this);
+		Rubyore = new Rubyore(this);
+		Thermiteore = new Thermiteore(this);
+		Crystaliteore = new Crsytaliteore(this);
 		log.info("[Mo Ore's] Is Enabled!"); 
-		SpoutManager.getFileManager().addToPreLoginCache(plugin, url);
 	}
 	 
 	public void onDisable(){ 
