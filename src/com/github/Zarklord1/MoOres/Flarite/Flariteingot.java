c3a504b8fe012629f61064c9f171cd0ebd286220
Zachary Lockwood <bestgoaltenderever@gmail.com>
2012-04-22 11:39:44 -0400

    added item:Flarite ingot

--- src/com/github/Zarklord1/MoOres/Flarite/Flariteingot.java
+++ src/com/github/Zarklord1/MoOres/Flarite/Flariteingot.java
@@ -1,5 +1,13 @@
 package com.github.Zarklord1.MoOres.Flarite;
 
-public class Flariteingot {
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.material.item.GenericCustomItem;
 
+public class Flariteingot extends GenericCustomItem  {
+
+	public Flariteingot(Plugin plugin, String name, String texture) {
+		super(plugin, "Flarite Ingot",  "http://cdn.spout.org/img/icon/spout_16x16.png");
+		SpoutManager.getFileManager().addToPreLoginCache(plugin,  "http://cdn.spout.org/img/icon/spout_16x16.png");
+	}
 }
