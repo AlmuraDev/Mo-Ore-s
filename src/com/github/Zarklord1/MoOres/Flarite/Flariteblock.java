221cf018cb60da3e9dfc2d776cf251b7d825afe9
Zachary Lockwood <bestgoaltenderever@gmail.com>
2012-04-24 16:56:45 -0400

    added main blocks

---
+++ src/com/github/Zarklord1/MoOres/Flarite/Flariteblock.java
@@ -0,0 +1,41 @@
+package com.github.Zarklord1.MoOres.Flarite;
+
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.plugin.Plugin;
+import org.getspout.commons.block.BlockFace;
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+public class Flariteblock extends GenericCubeCustomBlock {
+	public Flariteblock(Plugin plugin) {
+        super(plugin, "Flarite Block", "http://cdn.spout.org/img/icon/spout_16x16.png", 16);
+        SpoutManager.getFileManager().addToPreLoginCache(plugin,  "http://cdn.spout.org/img/icon/spout_16x16.png");
+	}
+	 
+    public void onNeighborBlockChange(World world, int x, int y, int z, int changedId) { }
+ 
+    public void onBlockPlace(World world, int x, int y, int z) { }
+ 
+    public void onBlockPlace(World world, int x, int y, int z, LivingEntity living) { }
+ 
+    public void onBlockDestroyed(World world, int x, int y, int z) { }
+ 
+    public boolean onBlockInteract(World world, int x, int y, int z, SpoutPlayer player) {
+        return true;
+    }
+ 
+    public void onEntityMoveAt(World world, int x, int y, int z, Entity entity) { }
+ 
+    public void onBlockClicked(World world, int x, int y, int z, SpoutPlayer player) { }
+ 
+    public boolean isProvidingPowerTo(World world, int x, int y, int z, BlockFace face) {
+        return false;
+    }
+ 
+    public boolean isIndirectlyProvidingPowerTo(World world, int x, int y, int z, BlockFace face) {
+        return false;
+    }
+}
--- src/com/github/Zarklord1/MoOres/MoOres.java
+++ src/com/github/Zarklord1/MoOres/MoOres.java
@@ -3,7 +3,6 @@ package com.github.Zarklord1.MoOres;
 import java.util.logging.Logger;
 
 
-import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
@@ -12,7 +11,6 @@ import org.getspout.spoutapi.inventory.SpoutShapelessRecipe;
 import org.getspout.spoutapi.material.CustomBlock;
 import org.getspout.spoutapi.material.CustomItem;
 import org.getspout.spoutapi.material.MaterialData;
-
 import com.github.Zarklord1.MoOres.Crystalite.*;
 import com.github.Zarklord1.MoOres.Flarite.*;
 import com.github.Zarklord1.MoOres.Ruby.*;
@@ -27,6 +25,11 @@ public class MoOres extends JavaPlugin{
 	public static CustomBlock Rubyore;
 	public static CustomBlock Thermiteore;
 	public static CustomBlock Crystaliteore;
+	public static CustomBlock FlariteBlock;
+	public static CustomBlock SilverBlock;
+	public static CustomBlock RubyBlock;
+	public static CustomBlock ThermiteBomb;
+	public static CustomBlock CrystaliteBlock;
 	public static CustomItem Flariteingot;
 	public static CustomItem Crystalite;
 	public static CustomItem Ruby;
@@ -42,6 +45,11 @@ public class MoOres extends JavaPlugin{
 		Rubyore = new Rubyore(this);
 		Thermiteore = new Thermiteore(this);
 		Crystaliteore = new Crsytaliteore(this);
+		FlariteBlock = new Flariteblock(this);
+		SilverBlock = new Silverblock(this);
+		RubyBlock = new Rubyblock(this);
+		ThermiteBomb = new Thermitebomb(this);
+		CrystaliteBlock = new Crystaliteblock(this);
 		Flariteingot = new Flariteingot(this);
 		Crystalite = new Crystalite(this);
 		Ruby = new Ruby(this);
@@ -59,9 +67,46 @@ public class MoOres extends JavaPlugin{
 		log.info("[Mo Ores] Registered Custom Drops!");
 		
 		log.info("[Mo Ores] Registering Recipes...");
-
-		log.info("[Mo Ores] Registered all Recipes!");
+		SpoutShapelessRecipe Flaritefurningot1 = new SpoutShapelessRecipe( new SpoutItemStack(Flariteingot, 1));
+		Flaritefurningot1.addIngredient(Flariteore);
+		Flaritefurningot1.addIngredient(1, MaterialData.stick);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Flaritefurningot1);
 		
+		SpoutShapelessRecipe Flaritefurningot8 = new SpoutShapelessRecipe( new SpoutItemStack(Flariteingot, 8));
+		Flaritefurningot8.addIngredient(8, Flariteore);
+		Flaritefurningot8.addIngredient(MaterialData.coal);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Flaritefurningot8);
+		
+		SpoutShapelessRecipe Silverfurningot1 = new SpoutShapelessRecipe( new SpoutItemStack(Silveringot, 1));
+		Silverfurningot1.addIngredient(Silverore);
+		Silverfurningot1.addIngredient(2, MaterialData.stick);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Silverfurningot1);
+		
+		SpoutShapelessRecipe Silverfurningot8 = new SpoutShapelessRecipe( new SpoutItemStack(Silveringot, 8));
+		Silverfurningot8.addIngredient(8, Silverore);
+		Silverfurningot8.addIngredient(MaterialData.coal);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Silverfurningot8);
+		
+		SpoutShapedRecipe Crystaliteblock = new SpoutShapedRecipe(new SpoutItemStack(CrystaliteBlock, 1));
+		Crystaliteblock.shape("ccc", "ccc", "ccc");
+		Crystaliteblock.setIngredient('c', Crystalite);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Crystaliteblock);
+		
+		SpoutShapedRecipe Flariteblock = new SpoutShapedRecipe(new SpoutItemStack(FlariteBlock, 1));
+		Flariteblock.shape("ccc", "ccc", "ccc");
+		Flariteblock.setIngredient('c', Flariteingot);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Flariteblock);
+		
+		SpoutShapedRecipe Silverblock = new SpoutShapedRecipe(new SpoutItemStack(SilverBlock, 1));
+		Silverblock.shape("ccc", "ccc", "ccc");
+		Silverblock.setIngredient('c', Silveringot);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Silverblock);
+		
+		SpoutShapedRecipe Rubyblock = new SpoutShapedRecipe(new SpoutItemStack(RubyBlock, 1));
+		Rubyblock.shape("ccc", "ccc", "ccc");
+		Rubyblock.setIngredient('c', Ruby);
+		SpoutManager.getMaterialManager().registerSpoutRecipe(Rubyblock);
+		log.info("[Mo Ores] Registered all Recipes!");
 		log.info("[Mo Ores] Is Enabled!");
 	}
 	 
