90fcf80b711e046a467035c9d8124832ec9bee05
Zachary Lockwood <bestgoaltenderever@gmail.com>
2012-05-07 13:53:23 -0400

    Netbeans commit

---
+++ src/com/github/Zarklord1/MoOres/Custom/Items/Bowplusarrow/GenericCustomArrow.java
@@ -0,0 +1,12 @@
+package com.github.Zarklord1.MoOres.Custom.Items.Bowplusarrow;
+
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.material.item.GenericCustomItem;
+
+public class GenericCustomArrow extends GenericCustomItem  {
+
+	public GenericCustomArrow(Plugin plugin, String name, String texture, int damage) {
+		super(plugin, name, texture);
+	}
+
+}
---
+++ src/com/github/Zarklord1/MoOres/Custom/Items/Bowplusarrow/GenericCustomBow.java
@@ -0,0 +1,13 @@
+package com.github.Zarklord1.MoOres.Custom.Items.Bowplusarrow;
+
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.material.item.GenericCustomItem;
+
+public class GenericCustomBow extends GenericCustomItem {
+
+	public GenericCustomBow(Plugin plugin, String name, String texture, double Charge) {
+		super(plugin, name, texture);
+	}
+
+
+}
